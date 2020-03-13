ad852506e1118fdfad34a5160ed108a730fc16e7
RuanChistopher <50343643+RuanChistopher@users.noreply.github.com>
2020-03-13 20:39:01 -0300

    Iniciando da Api

---
+++ src/app/servicos/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = "https://reqres.in/api/users";
+  constructor(private http: HttpClient) { 
+  }
+  public listaUsuarios(pagina: Number){
+    return this.http.get(`${this.url}?page=${pagina}`);
+  }
+
+  public buscaUsuario(id: Number){
+    return this.http.get(`${this.url}/${id}`);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { UserService } from '../servicos/user.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page { 
 
-  constructor() {}
+  //Para paginação
+  public page = 1;
+  public totalPaginas = 0;
+
+  public totalUsuarios =0;
+  public usuariosPagina = 0;
+
+  // Para guardar á lista de usuários
+  public listaUsuarios =[];
+
+  constructor(private userService: UserService) {
+    this.buscarUsuario(this.page);
+  }
+
+  public buscarUsuario(pagina: Number){
+    this.userService.listaUsuarios(pagina).subscribe(dados => {
+        this.page = dados['page'];
+        this.totalPaginas = dados['total_page'];
+        this.usuariosPagina = dados['per_page'];
+        this.totalUsuarios = dados['total'];
+        this.listaUsuarios = dados['data'];
+    })
+  }
 
 }
