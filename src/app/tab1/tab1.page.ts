188efca268c904b81e48240bff646d651f1be33a
RuanChistopher <50343643+RuanChistopher@users.noreply.github.com>
2020-03-16 22:25:32 -0300

    Paginação API/Usuário

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -25,7 +25,7 @@ export class Tab1Page {
   public buscarUsuario(pagina: Number){
     this.userService.listaUsuarios(pagina).subscribe(dados => {
         this.page = dados['page'];
-        this.totalPaginas = dados['total_page'];
+        this.totalPaginas = dados['total_pages'];
         this.usuariosPagina = dados['per_page'];
         this.totalUsuarios = dados['total'];
         this.listaUsuarios = dados['data'];
